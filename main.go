4c69e21527cbcff4d52113954e7bc4226c1c5133
mm-masahiro <hand5.mm9.mzh@gmail.com>
2023-02-06 21:46:09 +0900

    chap2

--- main.go
+++ main.go
@@ -2,7 +2,23 @@ package main
 
 import "fmt"
 
+const x int64 = 10
+
+const (
+	idKey = "id"
+	nameKey = "name"
+)
+
+const z = 20 * 10
+
 func main() {
-	fmt.Println("Hello, World")
+  const y = "Hello"
+
+	fmt.Println(x)
+	fmt.Println(y)
+
+	fmt.Println(x)
+	fmt.Println(y)
+
 }
 
