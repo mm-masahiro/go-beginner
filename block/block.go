eb6dcbb90e62944fb49789c6931d6422131909d9
mm-masahiro <hand5.mm9.mzh@gmail.com>
2023-09-09 09:17:34 +0900

    go if statement

--- block/block.go
+++ block/block.go
@@ -1,6 +1,10 @@
 package block
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func Block() {
 	x := 10
@@ -15,4 +19,15 @@ func Block() {
 
 	fmt.Println(x)
 	// -> printed 10
+
+	rand.Seed(time.Now().UnixNano())
+	// Goでは、ifのあとにこのブロックの中だけで有効な変数を定義することができる！！
+	// if n := rand.Intn(10); n == 0 {
+	if n := 0; n == 0 {
+		fmt.Println("n  is 0")
+	} else if n < 10 {
+		fmt.Println("n is greater than 0 but, smaller than 10")
+	} else {
+		fmt.Println("n is greater than 10")	
+	}
 }
